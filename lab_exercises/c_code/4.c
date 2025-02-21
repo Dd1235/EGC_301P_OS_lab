12d43daa40df9e2595abe2dcce4dd60f5f1469b8
Dedeepya Avancha <deepya1235@gmail.com>
2025-02-21 12:14:00 +0530

    add 4 and 5

---
+++ lab_exercises/c_code/4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h> // define the some of the requests and arguments for use by the functions fcntl() and open()
+#include <sys/stat.h>
+
+int main()
+{
+    // O_EXCL makes sure that if the file already exists, open() will fail
+    // so this now gives you error if file already exists, EEXIST because of O_EXCL
+    // success when file is created
+    int fd = open("../sample.txt", O_RDWR | O_CREAT | O_EXCL, 0744); // permissions matter when creating a new file
+    if (fd == -1)
+    {
+        perror("Error");
+        exit(1);
+    }
+    else
+    {
+        printf("File opened successfully, its file descriptor: %d \n", fd);
+    }
+    close(fd);
+    return 0;
+}
---
+++ lab_exercises/c_code/5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+    int num_files = 5;
+    for (int i = 1; i <= num_files; i++)
+    {
+        char filename[20];
+        sprintf(filename, "file_5_%d.txt", i);
+        int fd = open(filename, O_RDWR | O_CREAT | O_EXCL, 0744);
+        if (fd == -1)
+        {
+            perror("Error");
+            // exit(1);
+        }
+        else
+        {
+            printf("File opened successfully, its file descriptor: %d \n", fd);
+        }
+    }
+    while (1)
+    {
+        // infinite loop
+    }
+}
