91c51e3c1019c16c9565e173e57b0c14a069372d
Dedeepya Avancha <deepya1235@gmail.com>
2025-04-04 13:22:32 +0530

    add comennts, to do form 31 to 40 now

--- lab_exercises/c_code/23.c
+++ lab_exercises/c_code/23.c
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+// you will notice that bash/zsh will print the $/% base prompt after parent exits because bash created parent
+// so even with 3 seconds of child process running bash will print this off
+// please wait for 3 seconds after parent exits to see the prints of the child
+// and when the child exits you will not observe the default shell prompt being printed because bash does not track this child terminating.
 int main()
 {
     pid_t child_pid = fork();
@@ -21,11 +25,16 @@ int main()
 
         sleep(5);
 
-        printf("[Child] After sleep, my new parent PID (should be 1): %d\n", getppid());
+        // if you are not using mac, but Linux, the reparented PID will not be 1
+        // 1 if root user systemd
+        // you will get systemd of not root user, but the user that is running the program.
+        // so not 1, but some otehr number
+        printf("[Child] After sleep, my new parent PID (should be 1 in mac, and, the PID of systemd of user running it in Linux): %d\n", getppid());
         printf("[Child] I have become an orphan and was reparented to init/systemd.\n");
         fflush(stdout);
 
-        exit(0);
+        // exit(0);
+        return 0;
     }
     else
     {
